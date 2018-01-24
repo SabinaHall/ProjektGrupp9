fe63176f8da51be50c0c97b54815798ceb0e67ba
Henric Berg <henric_berg@live.se>
2018-01-24 15:43:47 +0100

    mini-övrsättningspush

--- ProjektGrupp9/Domain/Controllers/HomeController.cs
+++ ProjektGrupp9/Domain/Controllers/HomeController.cs
@@ -15,14 +15,14 @@ namespace Domain.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Vår 'about'-sida.";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Vår kontaktsida.";
 
             return View();
         }
