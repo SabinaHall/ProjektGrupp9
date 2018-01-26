971c0be23fc673dbb13250656569664788054e66
robinkohn <x.robin@live.se>
2018-01-26 14:08:00 +0100

    Funktion för att skicka mötesförfrågan till folk klart, ska bara fixa indexes

---
+++ ProjektGrupp9/DataLogic/Models/EventViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLogic.Models
+{
+    public class EventViewModel
+    {
+        public Events events { get; set; }
+        public List<ApplicationUser> users { get; set; }
+
+    }
+}
