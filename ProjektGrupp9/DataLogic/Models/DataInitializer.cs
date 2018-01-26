acc1827a8dc6012660ab42d1071f9aeef1c4b797
Christoffer Elmesiöö <Christoffer Elmesi@DESKTOP-UM3CRHL>
2018-01-26 09:22:12 +0100

    Registrera användare med roller

--- ProjektGrupp9/DataLogic/Models/DataInitializer.cs
+++ ProjektGrupp9/DataLogic/Models/DataInitializer.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
@@ -9,6 +11,57 @@ namespace DataLogic.Models
 {
     public class DataInitializer : DropCreateDatabaseIfModelChanges<ApplicationDbContext>
     {
+        protected override void Seed(ApplicationDbContext context)
+        {
 
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+
+
+            // In Startup iam creating first Admin Role and creating a default Admin User    
+            if (!roleManager.RoleExists("SuperAdmin"))
+            {
+
+                // first we create Admin rool   
+                var role = new IdentityRole();
+                role.Name = "SuperAdmin";
+                roleManager.Create(role);
+            }
+
+
+            if (!context.Users.Any(x => x.UserName == "andreas@live.se"))
+            {
+                PasswordHasher pwdHash = new PasswordHasher();
+                string pwd = pwdHash.HashPassword("123");
+                var user = new ApplicationUser();
+
+
+                user.PasswordHash = pwd;
+                user.UserName = "andreas@live.se";
+                user.Email = "andreas@live.se";
+                user.SecurityStamp = Guid.NewGuid().ToString(); //THIS IS WHAT I NEEDED
+                context.Users.Add(user);
+                context.SaveChanges();
+
+                UserManager.AddToRole(user.Id, "SuperAdmin");
+            }
+
+            // creating Creating Manager role    
+            if (!roleManager.RoleExists("Admin"))
+            {
+                var role = new IdentityRole();
+                role.Name = "Admin";
+                roleManager.Create(role);
+            }
+
+            // creating Creating Employee role    
+            if (!roleManager.RoleExists("User"))
+            {
+                var role = new IdentityRole();
+                role.Name = "User";
+                roleManager.Create(role);
+            }
+            base.Seed(context);
+        }
     }
 }
