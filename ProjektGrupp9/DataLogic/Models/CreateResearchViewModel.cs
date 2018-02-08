07a37ea593fb6bac7f7a76c62485b99a9485dda7
jr7963 <jr7963@gmail.com>
2018-02-08 12:45:01 +0100

    tagga forskning

---
+++ ProjektGrupp9/DataLogic/Models/CreateResearchViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace DataLogic.Models
+{
+    public class CreateResearchViewModel
+    {
+        [Display(Name = "Välj taggar")]
+        public List<EntryTag> TagList { get; set; }
+
+        public IEnumerable<SelectListItem> TagNameList { get; set; }
+
+        [Required]
+        public List<string> SelectedTagIds { get; set; }
+
+        public EntryResearch Entries { get; set; }
+
+        public virtual ApplicationUser Author { get; set; }
+
+    }
+}
